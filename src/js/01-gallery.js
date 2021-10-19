b3fdee0e8f59210030061f39cd818a3f40917955
VitaliiKovalenko88 <vintilyator88@gmail.com>
2021-10-19 21:18:07 +0200

    зарендерил разметку галереи

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,4 +1,20 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const markupItemsGallary = ({ original, preview, description }) => {
+  return `<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+};
+
+const makeItemsGallery = (img) => img.map(markupItemsGallary).join("");
+
+const listGalleryEl = document.querySelector(".gallery");
+listGalleryEl.insertAdjacentHTML("beforeend", makeItemsGallery(galleryItems));
