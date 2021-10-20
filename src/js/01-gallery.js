36a7e1240fa97a6a719d7ae7a96f5a64e4503775
VitaliiKovalenko88 <vintilyator88@gmail.com>
2021-10-21 00:47:39 +0200

    сделан функционал согласно тз

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,20 +1,48 @@
 import { galleryItems } from "./gallery-items.js";
 // Change code below this line
+// import * as basicLightbox from "basiclightbox";
 
-const markupItemsGallary = ({ original, preview, description }) => {
-  return `<div class="gallery__item">
-  <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</div>`;
+const refs = {
+  gelleryEl: document.querySelector(".gallery"),
 };
 
-const makeItemsGallery = (img) => img.map(markupItemsGallary).join("");
+const createItemsGallaryMarkup = (img) =>
+  img
+    .map(
+      ({ original, preview, description }) =>
+        `<div class="gallery__item">
+           <a class="gallery__link" href="${original}">
+             <img
+               class="gallery__image"
+               src="${preview}"
+               data-source="${original}"
+               alt="${description}"
+             />
+           </a>
+         </div>`
+    )
+    .join("");
 
-const listGalleryEl = document.querySelector(".gallery");
-listGalleryEl.insertAdjacentHTML("beforeend", makeItemsGallery(galleryItems));
+refs.gelleryEl.insertAdjacentHTML(
+  "beforeend",
+  createItemsGallaryMarkup(galleryItems)
+);
+
+refs.gelleryEl.addEventListener("click", (e) => {
+  e.preventDefault();
+  const currentImg = e.target;
+  const isGalleryImg = currentImg.classList.contains("gallery__image");
+  if (!isGalleryImg) {
+    return;
+  }
+  const currentUrl = currentImg.dataset.source;
+  onOpenModalImage(currentUrl);
+});
+
+const onOpenModalImage = (url) => {
+  const instance = basicLightbox.create(`
+    <img src="${url}" width="800" height="600">
+`);
+  instance.show();
+  console.dir(instance.element);
+};
